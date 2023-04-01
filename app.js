0a063b17d5f16bd865d34c199d680d2c9a19f299
Txinkang <2912528586@qq.com>
2023-04-01 12:24:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.port||3000;
+const port = 80;
 app.get('/api', (req, res) => {
     const data = {
         name: 'Bing',
